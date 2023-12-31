3b590323784c1afb60cbd7f62ffc80baab3c1a32
Matheus Salomao <mathesbh@gmail.com>
2023-12-31 13:06:13 -0300

    feat: inclui db adapter

---
+++ DbAdapter/CriptomoedaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbAdapter
+{
+    public class CriptomoedaDbContext
+    {
+        public DbSet<HistoricoNegociacoes> HistoricoNegociacoes { get; set; }
+        public DbSet<HistoricoNegociacao> HistoricoNegociacao { get; set; }
+    }
+}
