974b65650ee39402d928b442df0eaf045950c96e
Matheus Salomao <mathesbh@gmail.com>
2024-01-04 20:05:07 -0300

    chore: inclui interfaces para obter negociações e salvar histórico

---
+++ Domain/Adapters/IDbHistoricoNegociacoesAdapter.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+
+namespace Domain.Adapters
+{
+    public interface IDbHistoricoNegociacoesAdapter
+    {
+        /// <summary>
+        ///     Salva os dados das últimas 24 horas de negociações de uma criptomoeda especifica.
+        /// </summary>
+        /// <param name="dadosNegociacoes">
+        ///     Dados da negociações.
+        /// </param>
+        /// <param name="siglaMoeda">
+        ///     Sigla da criptomoeda que deseja obter dados.
+        /// </param>
+        Task SalvarHistoricoNegociacoesAsync(NegociacoesDoDia dadosNegociacoes, string siglaMoeda);
+    }
+}
--- Domain/Services/ICriptoMoedaService.cs
+++ Domain/Services/ICriptoMoedaService.cs
@@ -12,5 +12,13 @@ namespace Domain.Services
         ///     Informações com o resumo das últimas 24 horas de negociações.
         /// </returns>
         Task<NegociacoesDoDia> ObterDadosNegociacoesDoDiaAsync(string siglaMoeda);
+        /// <summary>
+        ///     Dados das últimas 24 horas de negociações de uma criptomoeda especifica e salva dados.
+        /// </summary>
+        /// <param name="siglaMoeda">Sigla da criptomoeda que deseja obter dados.</param>
+        /// <returns>
+        ///     Informações com o resumo das últimas 24 horas de negociações.
+        /// </returns>
+        Task<NegociacoesDoDia> ObterDadosNegociacoesDoDiaComHistoricoAsync(string siglaMoeda);
     }
 }
