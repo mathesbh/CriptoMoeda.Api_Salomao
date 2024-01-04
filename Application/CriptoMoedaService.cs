df62f72092755c1d53bf4718459d8657fdc0c824
Matheus Salomao <mathesbh@gmail.com>
2024-01-04 20:07:46 -0300

    chore: inclui metodo para salvar historico no serviço cripto

--- Application/CriptoMoedaService.cs
+++ Application/CriptoMoedaService.cs
@@ -7,19 +7,38 @@ namespace Application
     public class CriptoMoedaService : ICriptoMoedaService
     {
         private readonly IMercadoBitcoinAdapter mercadoBitcoinAdapter;
-        public CriptoMoedaService(IMercadoBitcoinAdapter mercadoBitcoinAdapter)
+        private readonly IDbHistoricoNegociacoesAdapter dbHistoricoNegociacoesAdapter;
+        public CriptoMoedaService(IMercadoBitcoinAdapter mercadoBitcoinAdapter, IDbHistoricoNegociacoesAdapter dbHistoricoNegociacoesAdapter)
         {
             this.mercadoBitcoinAdapter = mercadoBitcoinAdapter ??
                 throw new ArgumentNullException(nameof(mercadoBitcoinAdapter));
+            this.dbHistoricoNegociacoesAdapter = dbHistoricoNegociacoesAdapter ??
+                throw new ArgumentNullException(nameof(dbHistoricoNegociacoesAdapter));
         }
         public async Task<NegociacoesDoDia> ObterDadosNegociacoesDoDiaAsync(string siglaMoeda)
         {
-            if (string.IsNullOrWhiteSpace(siglaMoeda))
-            {
-                throw new Exception("Sigla escolhida não é valida.");
-            }
+            ValidaSiglaMoeda(siglaMoeda);
 
             return await mercadoBitcoinAdapter.ObterDadosNegociacoesDoDiaAsync(siglaMoeda);
+        }
+
+        public async Task<NegociacoesDoDia> ObterDadosNegociacoesDoDiaComHistoricoAsync(string siglaMoeda)
+        {
+            ValidaSiglaMoeda(siglaMoeda);
+
+            var dadosNegociacoes = await mercadoBitcoinAdapter.ObterDadosNegociacoesDoDiaAsync(siglaMoeda);
+
+            await dbHistoricoNegociacoesAdapter.SalvarHistoricoNegociacoesAsync(dadosNegociacoes, siglaMoeda);
+
+            return dadosNegociacoes;
+        }
+
+        private void ValidaSiglaMoeda(string siglaMoeda) 
+        {
+            if (string.IsNullOrWhiteSpace(siglaMoeda))
+            {
+                throw new ArgumentException("Sigla escolhida não é valida.");
+            }
         }
     }
 }
